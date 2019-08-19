233c6267f6e6f601a78a1911ace1dd0cfb62507f
valik <valentin.pazushko@tut.by>
2019-08-19 13:07:00 +0300

    reverted news auto publication

--- api/components/parser/AbstractParser.php
+++ api/components/parser/AbstractParser.php
@@ -106,7 +106,9 @@ abstract class AbstractParser
 
                 try
                 {
-                    $model = Factory::saveNews($meta['title'], $this->prepareText($meta['text']), false);
+                    $is_publish = (int)(count($meta['companies']) > 0);
+
+                    $model = Factory::saveNews($meta['title'], $this->prepareText($meta['text']), $is_publish);
 
                     Factory::linkCompanies($model, $meta['companies']);
 
